c37ce78e1ac4799e85c4d0b6371706546390cf0a
Danielius98 <150250346+Danielius98@users.noreply.github.com>
2024-02-14 19:28:00 +0700

    Add files via upload

--- ConsoleApp2/TemperatureConverter.cs
+++ ConsoleApp2/TemperatureConverter.cs
@@ -3,84 +3,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
 
-    public class TemperatureConverter
+
+    // Метод для конвертации температуры
+  using System;
+
+public class TemperatureConverter
+{
+    public double ConvertTemperature(double temperature, string sourceUnit, string targetUnit)
     {
-        public double ConvertTemperature(double temperature, string sourceUnit, string targetUnit)
+        if (sourceUnit.ToUpper() == "K" && temperature < 0)
         {
-            if (temperature < 0)
-            {
-                throw new ArgumentException("Некорректное значение температуры. Температура не может быть отрицательной.");
-            }
-
-            switch (sourceUnit.ToUpper())
-            {
-                case "C":
-                    if (targetUnit.ToUpper() == "F")
-                    {
-                        return CelsiusToFahrenheit(temperature);
-                    }
-                    else if (targetUnit.ToUpper() == "K")
-                    {
-                        return CelsiusToKelvin(temperature);
-                    }
-                    break;
-                case "F":
-                    if (targetUnit.ToUpper() == "C")
-                    {
-                        return FahrenheitToCelsius(temperature);
-                    }
-                    else if (targetUnit.ToUpper() == "K")
-                    {
-                        return FahrenheitToKelvin(temperature);
-                    }
-                    break;
-                case "K":
-                    if (targetUnit.ToUpper() == "C")
-                    {
-                        return KelvinToCelsius(temperature);
-                    }
-                    else if (targetUnit.ToUpper() == "F")
-                    {
-                        return KelvinToFahrenheit(temperature);
-                    }
-                    break;
-                default:
-                    throw new InvalidTemperatureException("Неверная единица измерения температуры.");
-            }
-
-            throw new InvalidOperationException("Неверная конвертация температуры.");
+            throw new ArgumentException("Недопустимое значение температуры. Температура по Кельвину не может быть отрицательной.");
         }
 
-        private double CelsiusToFahrenheit(double celsius)
+        switch (sourceUnit.ToUpper())
         {
-            return celsius * 9 / 5 + 32;
+            case "C":
+                if (targetUnit.ToUpper() == "F")
+                {
+                    return CelsiusToFahrenheit(temperature);
+                }
+                else if (targetUnit.ToUpper() == "K")
+                {
+                    return CelsiusToKelvin(temperature);
+                }
+                break;
+            case "F":
+                if (targetUnit.ToUpper() == "C")
+                {
+                    return FahrenheitToCelsius(temperature);
+                }
+                else if (targetUnit.ToUpper() == "K")
+                {
+                    return FahrenheitToKelvin(temperature);
+                }
+                break;
+            case "K":
+                if (targetUnit.ToUpper() == "C")
+                {
+                    return KelvinToCelsius(temperature);
+                }
+                else if (targetUnit.ToUpper() == "F")
+                {
+                    return KelvinToFahrenheit(temperature);
+                }
+                break;
+            default:
+                throw new InvalidTemperatureException("Недопустимая единица измерения температуры.");
         }
 
-        private double CelsiusToKelvin(double celsius)
-        {
-            return celsius + 273.15;
-        }
-
-        private double FahrenheitToCelsius(double fahrenheit)
-        {
-            return (fahrenheit - 32) * 5 / 9;
-        }
-
-        private double FahrenheitToKelvin(double fahrenheit)
-        {
-            return (fahrenheit - 32) * 5 / 9 + 273.15;
-        }
-
-        private double KelvinToCelsius(double kelvin)
-        {
-            return kelvin - 273.15;
-        }
-
-        private double KelvinToFahrenheit(double kelvin)
-        {
-            return (kelvin - 273.15) * 9 / 5 + 32;
-        }
+        throw new InvalidOperationException("Недопустимое преобразование температуры.");
     }
 
+    private double CelsiusToFahrenheit(double celsius)
+    {
+        return celsius * 9 / 5 + 32;
+    }
 
+    private double CelsiusToKelvin(double celsius)
+    {
+        return celsius + 273.15;
+    }
+
+    private double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    private double FahrenheitToKelvin(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9 + 273.15;
+    }
+
+    private double KelvinToCelsius(double kelvin)
+    {
+        return kelvin - 273.15;
+    }
+
+    private double KelvinToFahrenheit(double kelvin)
+    {
+        return (kelvin - 273.15) * 9 / 5 + 32;
+    }
+}
+
+public class InvalidTemperatureException : Exception
+{
+    public InvalidTemperatureException(string message) : base(message)
+    {
+    }
+}
